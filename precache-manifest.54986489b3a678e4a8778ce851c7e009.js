fbfa47d8bb7883fb683236b3f2ede306fc4dd6b2
sam smith <56478615+mihai-vatulescu13@users.noreply.github.com>
2020-03-23 19:31:29 +0200

    Updates

---
+++ precache-manifest.54986489b3a678e4a8778ce851c7e009.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8fda22b7288d0b45add04f35485f6c6",
+    "url": "/mihai-vatulescu13/Robofriends/index.html"
+  },
+  {
+    "revision": "f2d2b78a98183bbcaabf",
+    "url": "/mihai-vatulescu13/Robofriends/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "1e6bc7a8405bb7f1a3fc",
+    "url": "/mihai-vatulescu13/Robofriends/static/css/main.2cf4c7cf.chunk.css"
+  },
+  {
+    "revision": "f2d2b78a98183bbcaabf",
+    "url": "/mihai-vatulescu13/Robofriends/static/js/2.0710daf7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mihai-vatulescu13/Robofriends/static/js/2.0710daf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e6bc7a8405bb7f1a3fc",
+    "url": "/mihai-vatulescu13/Robofriends/static/js/main.3d60f997.chunk.js"
+  },
+  {
+    "revision": "ebe5f1fb7b3179656e54",
+    "url": "/mihai-vatulescu13/Robofriends/static/js/runtime-main.3a76ae32.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/mihai-vatulescu13/Robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
